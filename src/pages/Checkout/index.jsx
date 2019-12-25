1e1028810f6f450752a1a10ce8b142ab089a0b40
Pujit Mehrotra <pujitm2009@gmail.com>
2019-12-25 01:48:02 +0000

    Add price indicator block for desktop

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -33,14 +33,42 @@ class CheckoutPage extends Component {
         return (
             <div>
                 <TravelCitiesHeader origin="Boston → New York" />
-                <p>Your Total is {this.state.loading}</p>
+                {/* <p>Your Total is {this.state.loading}</p> */}
                 <div className="Checkout-Form-Wrapper">
                     <CheckoutForm
                         fName={query.get('fName') || ''}
                         lName={query.get('lName') || ''}
                         email={query.get('email') || ''}
                         paymentId={query.get('id') || ''}
-                    ></CheckoutForm>
+                    />
+                    <div className="Book-Price">
+                        <div style={{ minHeight: '196.333px' }}>
+                            <div style={{ transform: 'translateZ(0px)' }}>
+                                <div className='Book-Price-Block'>
+                                    <div>
+                                        <div className='Book-Price-h2'>Trip Price</div>
+                                        <div className='Book-Price-PassCount-Block'>
+                                            1 x Passenger
+                                            <i>${this.state.loading}</i>
+                                        </div>
+                                        <div className='Book-Price-PassCount-Block' style={{ marginTop: '10px' }}>
+                                            Baggage
+                                            <i>FREE</i>
+                                        </div>
+                                        <div className='Book-Price-Total'>
+                                            <div className='Book-Price-h1'>
+                                                <strong>
+                                                    Total to Pay
+                                                    <i>USD</i>
+                                                </strong>
+                                                <big className='Book-Price-Total-Default'>${this.state.loading}</big>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
