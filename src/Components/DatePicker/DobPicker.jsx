b017d46382007cf878e7772843972afc755ba172
Pujit Mehrotra <pujitm2009@gmail.com>
2020-01-30 13:42:34 -0500

    Remove date picker pop up #48dg3h[closed]

--- src/Components/DatePicker/DobPicker.jsx
+++ src/Components/DatePicker/DobPicker.jsx
@@ -1,46 +1,30 @@
-import React, { Fragment, useState } from "react";
-import { DatePicker } from "@material-ui/pickers";
+import React, { Fragment } from "react";
+import { KeyboardDatePicker } from "@material-ui/pickers";
 
 function DobPicker(props) {
-    // const [selectedDate, handleDateChange] = useState(new Date());
 
-    return (
-        <Fragment>
-            {/* <DatePicker
-        label="Basic example"
-        value={selectedDate}
-        onChange={handleDateChange}
-        animateYearScrolling
-      />
-
-      <DatePicker
-        autoOk
-        label="Clearable"
-        clearable
-        disableFuture
-        value={selectedDate}
-        onChange={handleDateChange}
-      /> */}
-
+  return (
+    <Fragment>
       {/* See https://stackoverflow.com/a/50432162 */}
       {/* https://material-ui.com/components/pickers/ */}
-
-            <DatePicker
-                disableFuture
-                openTo="year"
-                format="MM/dd/yyyy"
-                // label="Date of birth"
-                views={["year", "month", "date"]}
-                value={props.dob}
-                onChange={props.onChange}
-                fullWidth={true}
-                InputProps={{
-                    disableUnderline: true,
-                    margin: 'none',
-                }}
-            />
-        </Fragment>
-    );
+      <KeyboardDatePicker
+        disableToolbar
+        variant="inline"
+        format="MM/dd/yyyy"
+        id="date-picker-inline"
+        value={props.dob}
+        onChange={props.onChange}
+        KeyboardButtonProps={{
+          "aria-label": "change date"
+        }}
+        fullWidth={true}
+        InputProps={{
+          disableUnderline: true,
+          margin: "none"
+        }}
+      />
+    </Fragment>
+  );
 }
 
 export default DobPicker;
